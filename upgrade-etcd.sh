82ce6fbb71e3817307b64b8184be7531637d0005
626204 <kenneth.a.bass.ctr@mail.mil>
2023-02-26 21:35:06 -0500

    add validation steps to upgrade-etcd.sh

--- upgrade-etcd.sh
+++ upgrade-etcd.sh
@@ -10,9 +10,24 @@ DOWNLOAD_URL=${GOOGLE_URL}
 rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
 rm -rf /tmp/etcd-download-test && mkdir -p /tmp/etcd-download-test
 
+# Download upgrade binaries
 curl -L ${DOWNLOAD_URL}/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
 tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz -C /tmp/etcd-download-test --strip-components=1
 rm -f /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
 
+# Verify new version
 /tmp/etcd-download-test/etcd --version
 /tmp/etcd-download-test/etcdctl version
+
+# mv binaries to PATH
+sudo mv /tmp/etcd-download-test/etcd* /usr/local/bin/
+
+# Restart etcd service
+{   sudo systemctl daemon-reload;   sudo systemctl enable etcd;   sudo systemctl start etcd; }
+
+# Verify etcd service is running
+sudo systemctl status etcd
+
+# Verify etcd versions
+etcdctl version
+etcd --version
