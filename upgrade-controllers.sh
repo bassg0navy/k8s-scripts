1f27319056d4a17dc126ffcd54c2e5f38c9f30bf
Ubuntu <ubuntu@ip-10-240-0-10.ec2.internal>
2023-03-04 23:06:01 +0000

    fix array for loop syntax; tests pass on cp0

--- upgrade-controllers.sh
+++ upgrade-controllers.sh
@@ -29,7 +29,6 @@ KUBE_APISERVER_VERSION=$(kube-apiserver --version | (read v1 v2; echo $v2))
 KUBE_CONTROLLER_MANAGER_VERSION=$(kube-controller-manager --version | (read v1 v2; echo $v2))
 KUBE_SCHEDULER_VERSION=$(kube-scheduler --version | (read v1 v2; echo $v2))
 KUBECTL_VERSION=$(kubectl version -ojson | jq -r .clientVersion.gitVersion)
-declare -A assArray2=( [HDD]=Samsung [Monitor]=Dell [Keyboard]=A4Tech )
 
 declare -A VERSION_ARRAY=( [kube-apiserver]=$KUBE_APISERVER_VERSION \
     [kube-controller-manager]=$KUBE_CONTROLLER_MANAGER_VERSION \
@@ -47,26 +46,14 @@ echo "kubectl version:" $(kubectl version -ojson | jq -r .clientVersion.gitVersi
 
 
 # Determine success
-for key value in "${(kv)VERSION_ARRAY[@]}" 
+echo -e "\n"
+for service in "${!VERSION_ARRAY[@]}"; 
 do
-	if [[ $value == $K8S_VERSION ]]
+	if [[ ${VERSION_ARRAY[$service]} == $K8S_VERSION ]]
 	then
-            echo -e "$key version:" $value "Successful! 👍🏽  \n"
+            echo -e "$service version:" ${VERSION_ARRAY[$service]} "Successful! 👍🏽"
 	else
-        	echo -e "\n $key version:" $value "Upgrade Unsuccessful 💩 \n"
+        	echo -e "\n $service version:" ${VERSION_ARRAY[$service]} "Upgrade Unsuccessful 💩 "
 	fi
 done
-
-
-# for service_version in "${VERSION_ARRAY[@]}" 
-# do
-# 	if [[ $service_version == $K8S_VERSION ]]
-# 	then
-# 		for service in kube-apiserver kube-controller-manager kube-scheduler;
-# 		do
-# 			echo -e "$service version:" $($service --version) "Successful! 👍🏽  \n"
-#  		done
-# 	else
-#         	echo -e "\n $service_version Upgrade Unsuccessful 💩 \n"
-# 	fi
-# done
+echo -e "\n"
