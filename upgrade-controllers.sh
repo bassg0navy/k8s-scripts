22f26add1924ad1e3626b47d4c5d9076a34a3fd0
626204 <kenneth.a.bass.ctr@mail.mil>
2023-03-04 22:42:56 -0500

    ch/add variables(TARGET_VERSION,UPGRADE_LOG); empty log at start of script; + confirmation of service restart

--- upgrade-controllers.sh
+++ upgrade-controllers.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-K8S_VERSION="v1.24.0"
+# Declare variables
+TARGET_VERSION="v1.24.0"
+GOOGLE_URL=https://storage.googleapis.com
+DOWNLOAD_URL=${GOOGLE_URL}/kubernetes-release/release/${TARGET_VERSION}/bin/linux/amd64
 CONTROLLER_SERVICES=(kube-apiserver kube-controller-manager kube-scheduler kubectl)
 NEW_LINE=$(echo -e "\n")
+UPGRADE_LOG=/tmp/controller-upgrade-error.log
 
-# Choose either URL
-GOOGLE_URL=https://storage.googleapis.com
-GITHUB_URL=https://github.com/etcd-io/etcd/releases/download
-DOWNLOAD_URL=${GOOGLE_URL}/kubernetes-release/release/${K8S_VERSION}/bin/linux/amd64
+# Truncate old log
+truncate -s 0 $UPGRADE_LOG
 
 # Download binaries
 for service in "${CONTROLLER_SERVICES[@]}";
@@ -19,11 +21,21 @@ done
 chmod +x kube-apiserver kube-controller-manager kube-scheduler kubectl
 sudo mv kube-apiserver kube-controller-manager kube-scheduler kubectl /usr/local/bin/
 
+$NEW_LINE
+
 # Start controller services
-sudo systemctl daemon-reload && \
+echo "========== Restarting Controller Services =========="
+RESTART_SERVICES=$(sudo systemctl daemon-reload && \
 sudo systemctl enable kube-apiserver kube-controller-manager kube-scheduler && \
 sudo systemctl restart kube-apiserver kube-controller-manager kube-scheduler && \
-sudo systemctl status kube-apiserver kube-controller-manager kube-scheduler > /dev/null
+sudo systemctl status --full kube-apiserver kube-controller-manager kube-scheduler &> $UPGRADE_LOG)
+
+if [[ $RESTART_SERVICES ]]
+then
+    echo -e "All services restarted successfully!\n"
+else
+    echo -e "Service restarts unsuccessful. Please check $UPGRADE_LOG for more information\n"
+fi
 
 # Declare versions
 KUBE_APISERVER_VERSION=$(kube-apiserver --version | (read v1 v2; echo $v2))
@@ -41,7 +53,7 @@ $NEW_LINE
 # Determine success
 for service in "${!VERSION_ARRAY[@]}"; 
 do
-	if [[ ${VERSION_ARRAY[$service]} == $K8S_VERSION ]]
+	if [[ ${VERSION_ARRAY[$service]} == $TARGET_VERSION ]]
 	then
             echo -e "$service version:" ${VERSION_ARRAY[$service]} "Successful! 👍🏽"
 	else
